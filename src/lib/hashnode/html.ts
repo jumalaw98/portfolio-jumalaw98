cc1b7e4bb7eeae43f39caad3327395baa614fdf7
jumalaw98 <jumalawrence98@gmail.com>
2026-07-20 20:25:43 +0300

    feat: implement stripHtmlToText utility with HTML entity decoding and tag removal

--- src/lib/hashnode/html.ts
+++ src/lib/hashnode/html.ts
@@ -1,16 +1,32 @@
+const MAX_UNICODE_CODEPOINT = 0x10ffff;
+
+function safeFromCodePoint(codePoint: number): string {
+  if (Number.isNaN(codePoint) || codePoint < 0 || codePoint > MAX_UNICODE_CODEPOINT) {
+    return "\uFFFD"; // Unicode replacement character
+  }
+  return String.fromCodePoint(codePoint);
+}
+
+// HTML entity strings built to survive Prettier formatting
+const LT = "&" + "lt;";
+const GT = "&" + "gt;";
+const QUOT = "&" + "quot;";
+const APOS = "&" + "apos;";
+const AMP = "&" + "amp;";
+const NBSP = "&" + "nbsp;";
+const HASH_39 = "&" + "#39;";
+
 export function decodeHtmlEntities(text: string): string {
   return text
-    .replaceAll("&lt;", "<")
-    .replaceAll("&gt;", ">")
-    .replaceAll("&quot;", '"')
-    .replaceAll("&#39;", "'")
-    .replaceAll("&apos;", "'")
-    .replaceAll("&nbsp;", " ")
-    .replaceAll(/&#(\d+);/g, (_, dec) => String.fromCodePoint(Number.parseInt(dec, 10)))
-    .replaceAll(/&#[xX]([0-9a-fA-F]+);/g, (_, hex) =>
-      String.fromCodePoint(Number.parseInt(hex, 16)),
-    )
-    .replaceAll("&amp;", "&");
+    .replaceAll(LT, "<")
+    .replaceAll(GT, ">")
+    .replaceAll(QUOT, '"')
+    .replaceAll(HASH_39, "'")
+    .replaceAll(APOS, "'")
+    .replaceAll(NBSP, " ")
+    .replaceAll(/&#(\d+);/g, (_, dec) => safeFromCodePoint(Number.parseInt(dec, 10)))
+    .replaceAll(/&#[xX]([0-9a-fA-F]+);/g, (_, hex) => safeFromCodePoint(Number.parseInt(hex, 16)))
+    .replaceAll(AMP, "&");
 }
 
 export function stripHtmlToText(html: string): string {
